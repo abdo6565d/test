a034cef1c2902b4a889abc80a2440ce8de3fc092
Abdelrahman Alaa <abdo1212201212@gmail.com>
2022-05-14 17:18:40 +0200

    fourth edition

--- file.c
+++ file.c
@@ -3,11 +3,11 @@
 #define x 5
 #if x>y
 int main(){
-	printf("x value= %d",x);
+	printf("x va55= %d",x);
 }
 #elif x<y
 int main(){
-	printf("y value= %d",y);
+	printf("y value5= %d",y);
 }
 #else
 int main(){
